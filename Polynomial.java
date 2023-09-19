1c57dd59788ba034791349627c876fa790855f11
calvin-liew <calvin.y.liew@gmail.com>
2023-09-18 20:12:08 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,51 @@
+public class Polynomial {
+    double[] coefficients;
+
+    Polynomial(){
+        this.coefficients = new double[0];
+    }
+
+    Polynomial(double[] coefficients){
+        this.coefficients = coefficients;
+    }
+
+    Polynomial add(Polynomial polynomialToAdd){
+        double[] polynomialCo1 = this.coefficients;
+        double[] polynomialCo2 = polynomialToAdd.coefficients;
+
+        int polynomialCo1len = polynomialCo1.length;
+        int polynomialCo2len = polynomialCo2.length;
+
+        int length = Math.max(polynomialCo1len, polynomialCo2len);
+        double[] added = new double[length];
+
+        int index = 0;
+
+        while (index < length){
+            if(index < polynomialCo1len && index < polynomialCo2len){
+                added[index] = polynomialCo1[index] + polynomialCo2[index];
+            } else if (index >= polynomialCo1len) {
+                added[index] = polynomialCo2[index];
+            } else{
+                added[index] = polynomialCo1[index];
+            }
+            index++;
+        }
+        return new Polynomial(added);
+    }
+
+    Double evaluate(double x){
+        double[] coefficients = this.coefficients;
+        double result = 0;
+        for(int i = 0; i < coefficients.length; i++){
+            result += coefficients[i]*Math.pow(x, i);
+        }
+        return result;
+    }
+
+    boolean hasRoot(double x){
+        return evaluate(x) == 0;
+    }
+
+
+}
